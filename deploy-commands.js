869674cfa270ae85d3eada6ffb6a405b7046f49c
ykavatarleo-hash <ykavatarleo@gmail.com>
2026-04-03 13:13:19 +0100

    deploy-commands.js
    ticket cmd

---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+const { REST, Routes } = require("discord.js");
+
+const commands = [
+  {
+    name: "ticketpanel",
+    description: "Send the ticket panel"
+  }
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Registering commands...");
+
+    await rest.put(
+      Routes.applicationCommands("YOUR_CLIENT_ID"),
+      { body: commands }
+    );
+
+    console.log("Done!");
+  } catch (err) {
+    console.error(err);
+  }
+})();
