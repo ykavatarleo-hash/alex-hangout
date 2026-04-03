b0519d1fce344c3bb5dd9b89d089efa8895a2a4a
ykavatarleo-hash <ykavatarleo@gmail.com>
2026-04-03 19:24:42 +0100

    deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,23 +3,26 @@ const { REST, Routes } = require("discord.js");
 const commands = [
   {
     name: "ticketpanel",
-    description: "Send the ticket panel"
-  }
+    description: "Send the ticket panel",
+  },
 ];
+
+const clientId = "1479902342395596941"; // your bot ID
+const guildId = "1453937653539147820"; // your server ID
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log("Registering commands...");
+    console.log("Started refreshing application (/) commands...");
 
     await rest.put(
-      Routes.applicationGuildCommands("1479902342395596941", "1453937653539147820"),
+      Routes.applicationGuildCommands(clientId, guildId),
       { body: commands }
     );
 
-    console.log("Done!");
-  } catch (err) {
-    console.error(err);
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
   }
 })();
