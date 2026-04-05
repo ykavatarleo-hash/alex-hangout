ff1edd42651604c7d77ae9b9d0d882603e521e5e
ykavatarleo-hash <ykavatarleo@gmail.com>
2026-04-05 11:03:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,8 +31,37 @@ const CATEGORY_ID = "1453972934946459769";
 const TRANSCRIPT_CHANNEL = "1453974468547444819";
 const PANEL_CHANNEL = "1453944972477862136";
 
-client.once("ready", () => {
+});client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
+
+  client.user.setPresence({
+    activities: [{ name: "Tickets & Support", type: 3 }],
+    status: "online"
+  });
+
+  const { REST, Routes } = require("discord.js");
+
+  const commands = [
+    {
+      name: "ticketpanel",
+      description: "Send the ticket panel"
+    }
+  ];
+
+  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+  try {
+    console.log("Registering commands...");
+
+    await rest.put(
+      Routes.applicationGuildCommands("1479902342395596941", "1453937653539147820"),
+      { body: commands }
+    );
+
+    console.log("✅ Commands registered!");
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 client.on("interactionCreate", async (interaction) => {
